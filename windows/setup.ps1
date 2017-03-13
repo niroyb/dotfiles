59de1245aaf02648be0d9d1d989ab5a5a2bb42a3
Nick R <niroyb@gmail.com>
2017-03-12 23:16:18 -0700

    Add Stop on error to setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,4 +1,13 @@
 # Run in Admin Powershell
+
+# Stop on error
+$ErrorActionPreference = "Stop"
+
+# Allow to run downloaded scripts
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Confirm
+
+# Install chocolatey
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+# Disable chocolatey download confirmation
 choco feature enable -n allowGlobalConfirmation
